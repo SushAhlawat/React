9cc4beb8105d36795eba90d2d29e0d1d5baff482
Sushmita Ahlawat <susahlawat1998@gmail.com>
2020-01-10 14:22:03 +0530

    Control Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
@@ -11,6 +12,7 @@ import {COMMENTS} from '../shared/comments';
 import {LEADERS} from '../shared/leaders';
 import {PROMOTIONS} from '../shared/promotions';
 import {Switch, Route, Redirect} from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -54,6 +56,7 @@ class Main extends Component {
               <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
               <Route path = "/menu/:dishId" component = {DishWithId}/>
               <Route exact path = "/contactus" component = {Contact}/>
+              <Route path = "/aboutus" component = {() => <About leaders = {this.state.leaders}/>}/>
               <Redirect to="/home" />
       </Switch>
       <Footer/>
